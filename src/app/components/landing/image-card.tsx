521cd4d0da20d7fb131e04be5f233dff9e4594ea
Rishy-2021 <riteshpuwar2001@gmail.com>
2024-03-08 18:10:48 +0530

    add scrollable image card

---
+++ src/app/components/landing/image-card.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { Button, Image } from "antd";
+import { useState } from "react";
+import { FaChevronCircleLeft, FaChevronCircleRight } from "react-icons/fa";
+
+const ImageCard = () => {
+  const [state, setState] = useState<number[]>([1, 2, 3, 4, 5, 6]);
+
+  const onMoveForward = () => {
+    const val = state.map((val) => val + 1);
+    setState(val);
+  };
+  const onMoveBackward = () => {
+    const val = state.map((val) => (val !== 1 ? val - 1 : val));
+    setState(val);
+  };
+
+  return (
+    <div className="relative">
+      <div className="w-100vw flex justify-evenly flex-row h-500 px-5">
+        {state.map((val, idx) => (
+          <div className="mr-1.5">
+            <Image
+              src={`https://hotpot.ai/images/site/ai/headshot_maker/style_gallery/${val}.png`}
+              className="border-2"
+              key={idx}
+            />
+          </div>
+        ))}
+      </div>
+      <Button
+        disabled={state.includes(1)}
+        icon={<FaChevronCircleLeft size={25} />}
+        className="absolute inset-y-1/2 left-10 bg-green-200 -mt-4"
+        size="large"
+        onClick={onMoveBackward}
+      />
+      <Button
+        icon={<FaChevronCircleRight size={25} />}
+        className="absolute inset-y-1/2 right-10 -mt-4 bg-green-200"
+        size="large"
+        onClick={onMoveForward}
+      />
+    </div>
+  );
+};
+
+export default ImageCard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
-import AutoPlay from "./components/landing/image-carosol";
+import Activity from "./components/landing/activity";
+import HeadshotGenerate from "./components/landing/headshot-generate";
+import ImageCard from "./components/landing/image-card";
+import ImageCarosol from "./components/landing/image-carosol";
+import Pricing from "./components/landing/pricing";
+import StepsOfUsing from "./components/landing/setps-of-using";
 import TagLine from "./components/landing/tagline";
+import TrustedByCompanies from "./components/landing/trusted-by-companies";
 
 export default function Home() {
   return (
-    <div className="flex justify-center flex-col items-center">
+    <div className="flex justify-center flex-col items-center mb-20">
       <TagLine />
-      <AutoPlay />
+      <ImageCard />
+      <TrustedByCompanies />
+      <StepsOfUsing />
+      <HeadshotGenerate />
+      <Pricing />
+      <TrustedByCompanies />
+      <Activity />
     </div>
   );
 }
