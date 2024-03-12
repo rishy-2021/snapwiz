4645d8685030b5715ab6a79d076c66e1b29ec393
Rishy-2021 <riteshpuwar2001@gmail.com>
2024-03-12 14:19:33 +0530

    fix hidration error

--- components/modules/headshot-result.tsx
+++ components/modules/headshot-result.tsx
@@ -1,20 +1,34 @@
 import { Button, Image } from "antd";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaChevronCircleLeft, FaChevronCircleRight } from "react-icons/fa";
 import useScreenSize from "../global/useResizeHook";
 
 const HeadshotResult = () => {
     const screenSize = useScreenSize();
-    const [state, setState] = useState<number[]>(screenSize.width < 768 ? [1,2] : screenSize.width < 992 ? [1, 2, 3, 4] : [1,2, 3, 4, 5, 6] );
+    const [state, setState] = useState<number[]>([]);
 
     const onMoveForward = () => {
       const val = state.map((val) => (val < 15 ?val + 1 : val));
       setState(val);
     };
+
     const onMoveBackward = () => {
       const val = state.map((val) => (val !== 1 ? val - 1 : val));
       setState(val);
     };
+
+    console.log(screenSize.width, "widthh--------")
+
+    useEffect(()=>{
+      if(screenSize.width < 768){
+        setState([1,2])
+      } else if (screenSize.width < 992){
+        setState([1, 2, 3, 4])
+      }else {
+        setState([1,2, 3, 4, 5, 6])
+      }
+    },[screenSize.width])
+
    return (
     <div className="lg:w-2/3 flex items-center justify-center relative">
     <div className="w-11/12 flex items-center justify-center flex-row lg:px-5">
